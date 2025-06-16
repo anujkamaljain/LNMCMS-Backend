0f063dfd951a671e156d91868dbaac35f998fd86
anujkamaljain <23UCS540@lnmiit.ac.in>
2025-06-16 21:06:11 +0530

    API: Update CORS configuration to allow requests from the frontend application

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://lnmcms-frontend.vercel.app"],
     credentials: true,
   })
 );
